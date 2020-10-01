12669a874763001ab356f006d4fb3254c5c55a02
Kane <42565992+KaneTesta@users.noreply.github.com>
2020-10-01 17:41:49 +1000

    improved meta

--- src/App.js
+++ src/App.js
@@ -6,10 +6,23 @@ import './main.css';
 import Navbar from "./components/navbar.component";
 import KillerRandomPerks from "./components/killerperks.component";
 import SurvivorRandomPerks from "./components/survivorperks.component";
+import DocumentMeta from 'react-document-meta';
+
+const meta = {
+  title: 'Dead By Daylight - Perk Roulette',
+  description: 'Random perk generator for killers and survivors in the dead by daylight video game',
+  meta: {
+      charset: 'utf-8',
+      name: {
+          keywords: 'dead by daylight, random perk picker dead by daylight, dbd, dbd killers, dbd survivors'
+      }
+  }
+}
 
 function App() {
   return (
     <Router>
+      <DocumentMeta {...meta} />
       <div className="container">
         <Navbar />
         <br/>
